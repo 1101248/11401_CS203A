8717d44903d34fa8e15de27fa6f0bbb99f221806
1101248 <x10515710172@gmail.com>
2026-01-10 22:36:47 +0800

    Create hash_fn.h

---
+++ Assignment/AssignmentIV/C/hash_fn.h
@@ -0,0 +1,12 @@
+#ifndef HASH_FN_H
+#define HASH_FN_H
+
+int hash_division(int key, int table_size);
+
+
+int hash_mid_square(int key, int table_size);
+
+
+int hash_string_sum(const char* str, int table_size);
+
+#endif
